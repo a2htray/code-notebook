a77975a45f10a26d3a3dab3c8ceb2c70eb2acfca
a2htray <a2htray.yuen@gmail.com>
2023-03-03 13:39:05 +0800

    PAT basic level 1006 - 1013

---
+++ Algo/PAT/basic_1009.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: a2htray
+# create date: 2023/3/3
+
+"""
+PAT 乙级 1009
+"""
+
+if __name__ == '__main__':
+    statement = input().split(' ')
+    statement.reverse()
+    print(' '.join(statement))
