f1013d78c5b177dcbfbf453f731b278939d9d35a
a2htray <a2htray.yuen@gmail.com>
2023-05-09 09:49:30 +0800

    Go embed package

--- Go/gopkg/embed/main.go
+++ Go/gopkg/embed/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	_ "embed"
+	"embed"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 var (
 	//go:embed version.txt
 	version string
+	//go:embed version.txt
+	versionB []byte
+	//go:embed static/js/*
+	fs embed.FS
 )
 
 func main() {
-	fmt.Println(version)
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		b, _ := fs.ReadFile("static/js/app.js")
+
+		value := fmt.Sprintf(`version: %s
+versionB: %s
+app.js: %s
+`, version, string(versionB), string(b))
+
+		w.Write([]byte(value))
+	})
+
+	http.Handle("/static/", http.FileServer(http.FS(fs)))
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
